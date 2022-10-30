a194dadb0ec06419a1cfa0cd230126ea6252f28c
OskarWiedeweg <oskarwiedeweg01@gmail.com>
2022-10-30 20:47:16 +0100

    Added documentation to StopWatch

--- src/main/java/de/utopiamc/time/StopWatch.java
+++ src/main/java/de/utopiamc/time/StopWatch.java
@@ -1,7 +1,30 @@
 package de.utopiamc.time;
 
 import org.apache.log4j.Logger;
+import org.apiguardian.api.API;
 
+/**
+ * <h1>StopWatch</h1>
+ * <p>
+ * Should be used to log tasks with their used time. Tasks should be chained together.
+ * <p>
+ * E.g:
+ * <pre>
+ *     StopWatch watch = new StopWatch(logger);
+ *     watch.start();
+ *
+ *     // Task
+ *
+ *     watch.stopAndLog("Finished doing task.") // -> Console: Finished doing task. 20ms
+ *     watch.start();
+ *
+ *     // Next Task
+ *
+ *     watch.stopAndLog("Finished doing next task."); // -> Console: Finished doing next task. 34ms
+ *
+ * </pre>
+ */
+@API(status = API.Status.STABLE, since = "2")
 public final class StopWatch {
 
     public static final String ALREADY_STARTED_MSG = "Tried to start stopwatch, but it was already started!";
@@ -13,6 +36,10 @@ public final class StopWatch {
         this.logger = logger;
     }
 
+    /**
+     * Start the watch. This saves the {@link System#currentTimeMillis()}.
+     * @throws StopWatchException When the stopwatch is already started.
+     */
     public void start() {
         if (start != null) {
             throw new StopWatchException(ALREADY_STARTED_MSG);
@@ -20,6 +47,11 @@ public final class StopWatch {
         start = System.currentTimeMillis();
     }
 
+    /**
+     * Stops the watch and logs the message with the performed time in {@code ms}.
+     * @param msg Message that should be logged.
+     * @throws StopWatchException When the stopwatch is not started.
+     */
     public void stopAndLog(String msg) {
         if (start == null) {
             throw new StopWatchException(NOT_STARTED_MSG);
